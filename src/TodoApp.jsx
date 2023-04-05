388c73dfa5ff51f04ba5f7ba9978e15ed2348ec7
gopoma <gordono@unsa.edu.pe>
2023-04-05 11:34:48 -0500

    add theme

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,7 +1,10 @@
+import { AppTheme } from "./theme";
 import { TodoPage } from "./todos";
 
 export default function App() {
     return (
-        <TodoPage />
+        <AppTheme>
+            <TodoPage />
+        </AppTheme>
     );
 }
