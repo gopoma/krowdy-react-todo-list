ae9fcb9061abbfbbafa1488a1dff20ce29a69756
gopoma <gordono@unsa.edu.pe>
2023-04-13 00:41:04 -0500

    remove unused var

--- src/todos/views/TodosView.jsx
+++ src/todos/views/TodosView.jsx
@@ -21,7 +21,6 @@ import { useTodos } from "../hooks";
 import { TodoStatus, todosConfig } from "../constants";
 import { TodoForm } from "../components";
 
-const foo = 'bar';
 
 export default function TodosView() {
 	const {
@@ -106,7 +105,7 @@ export default function TodosView() {
 					fontSize={40}
 					fontWeight="bold"
 				>
-					todo.it
+					Listify
 				</Typography>
 				<Button
 					startIcon={<AddIcon />}
